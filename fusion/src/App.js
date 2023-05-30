a378527ef36a8f97e3fd1acb4a32c9706f539a51
levaverbuch <averbuch.lev@gmail.com>
2023-05-30 18:07:35 +0300

    form for fusion

--- fusion/src/App.js
+++ fusion/src/App.js
@@ -4,7 +4,7 @@ import FusionForm from "./components/forms/fusion_form";
 function App() {
   /*
   some notes:
-  The following code can be improved by making the input component fully  generic that response and act according to the input type.
+  The following code can be improved by making the input component fully generic, which responds and acts according to the input type.
   some of the validation behaviour was not clear by the instructions so I implemented in the UI as well as printing the error to the console.
    */
 
